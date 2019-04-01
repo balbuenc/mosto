97dba655bb79e567a4af19bd88b7779dda6f34b8
Mario <Mario@LAB-SQL>
2019-04-01 10:56:57 -0400

    ABM dependencias

---
+++ Almacenes/Dependencia.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Almacenes
+{
+    public partial class Dependencia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemInserted(object sender, FormViewInsertedEventArgs e)
+        {
+            Response.Redirect("Dependencia.aspx");
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Dependencia.aspx");
+        }
+
+        protected void GetRecordToUpdate(String ID)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection con = new SqlConnection(DependenciaDS.ConnectionString);
+
+            cmd = new SqlCommand("warehouse.[sp_Dependencia_get_Dependencia]", con);
+            cmd.Parameters.Add(new SqlParameter("@IdDependencia", ID));
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adp = new SqlDataAdapter();
+
+            con.Open();
+
+            adp.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+
+            EditFormView.DataSource = ds;
+            EditFormView.DataBind();
+
+            con.Close();
+        }
+
+
+        protected void DeleteRecord(String ID)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection con = new SqlConnection(DependenciaDS.ConnectionString);
+
+            cmd = new SqlCommand("warehouse.[sp_Dependencia_delete]", con);
+            cmd.Parameters.Add(new SqlParameter("@IdDependencia", ID));
+
+
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+
+
+            con.Close();
+        }
+
+
+        protected void ListView_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Editar")
+            {
+                GetRecordToUpdate(e.CommandArgument.ToString());
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "",
+               "$('#editModal').modal('show');", true);
+
+
+
+            }
+            else if (e.CommandName == "Eliminar")
+            {
+                DeleteRecord(e.CommandArgument.ToString());
+                DependenciaListView.DataBind();
+
+                ErrorLabel.Text = "El Registro se eliminó correctamente.";
+                ErrorLabel.Visible = true;
+                FadeOut(ErrorLabel.ClientID, 3000);
+            }
+        }
+
+
+        protected void FadeOut(string ClientID, int Time)
+        {
+            ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "script", "window.setTimeout(function() { document.getElementById('" + ClientID + "').style.display = 'none' }," + Time.ToString() + ");", true);
+        }
+
+
+
+        protected void EditFormView_ModeChanging(object sender, FormViewModeEventArgs e)
+        {
+            EditFormView.ChangeMode(e.NewMode);
+        }
+
+        protected void EditFormView_ItemUpdating(object sender, FormViewUpdateEventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            DataKey key = EditFormView.DataKey;
+
+            string format = "dd/MM/yyyy";
+
+            try
+            {
+                //Obtengo los valores de los campos a editar
+                TextBox txtIdDependencia = (TextBox)EditFormView.FindControl("txtIdDependencia");
+                TextBox txtDependencia = (TextBox)EditFormView.FindControl("txtDependencia");
+                TextBox txtDescripcion = (TextBox)EditFormView.FindControl("txtDescripcion");
+                DropDownList ActivoDDL = (DropDownList)EditFormView.FindControl("ActivoDDL");
+
+
+                //DateTime isoDateTime = DateTime.ParseExact(txtCalendar.Value, format, CultureInfo.InvariantCulture);
+
+                SqlConnection conn = new SqlConnection(DependenciaDS.ConnectionString);
+
+                cmd.Connection = conn;
+
+                cmd.CommandText = "warehouse.sp_Dependencia_update";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@IdDependencia", txtIdDependencia.Text);
+                cmd.Parameters.AddWithValue("@Dependencia", txtDependencia.Text);
+                cmd.Parameters.AddWithValue("@Descripcion", txtDescripcion.Text);
+                cmd.Parameters.AddWithValue("@Activo", ActivoDDL.SelectedValue);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "",
+                "$('#editModal').modal('hide');", true);
+
+                Response.Redirect("Dependencia.aspx");
+
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+                ErrorLabel.Visible = true;
+                FadeOut(ErrorLabel.ClientID, 5000);
+            }
+        }
+
+
+        protected void EditFormView_ItemUpdated(object sender, FormViewUpdatedEventArgs e)
+        {
+            EditFormView.ChangeMode(FormViewMode.ReadOnly);
+            ErrorLabel.Text = "El Registro de actualizò correctamente";
+            ErrorLabel.Visible = true;
+            FadeOut(ErrorLabel.ClientID, 5000);
+            DependenciaListView.DataBind();
+
+        }
+    }
+}

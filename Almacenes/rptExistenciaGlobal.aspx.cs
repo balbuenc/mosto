027ac8e96eb6ef4e0b5b546db54a9ea4565f3eda
Christian Balbuena <cbalbuena.cazzola@hotmail.com>
2019-07-10 07:34:13 -0400

    Reporte de Existencia Global

---
+++ Almacenes/rptExistenciaGlobal.aspx.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Almacenes
+{
+    public partial class rptExistenciaGlobal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Remote;
+                IReportServerCredentials irsc = new CustomReportCredentials(System.Configuration.ConfigurationManager.AppSettings["rptUSer"], System.Configuration.ConfigurationManager.AppSettings["rptUserKey"], System.Configuration.ConfigurationManager.AppSettings["rptUserDomain"]);
+                ReportViewer1.ServerReport.ReportServerCredentials = irsc;
+                ReportViewer1.ServerReport.ReportServerUrl = new Uri(System.Configuration.ConfigurationManager.AppSettings["ReportServer"]);
+                ReportViewer1.ServerReport.ReportPath = "/AlmacenesSSRS/ExistenciaGlobal";
+            }
+        }
+    }
+}
